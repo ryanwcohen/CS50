65cb4b4b3ff5b6012439c6657c4c27280564488f
Ryan W. Cohen <ryan.wade.cohen@gmail.com>
2011-06-27 21:11:51 -0400

    skittles.c

---
+++ guessing_game3.rb
@@ -0,0 +1,40 @@
+
+# cs50.tv pset1 skittles.c
+
+class Game
+attr_accessor :name, :target_number, :player_guess, :got_it_right, :counter
+
+def intro
+	puts "We're going to play a guessing game. What's your name?"
+	@name = gets.chomp
+	puts "Hi #{name}! I'm so glad to meet you. I'm thinking of a number between 0 and 100! Go ahead and guess what it is!" 
+end 
+
+def set_number
+	@target_number = rand(100)
+end 
+
+def check_for_win
+		
+	@counter = 0
+	@got_it_right = false
+	while @got_it_right == false do 
+	@counter += 1	
+	@player_guess = gets.to_i	
+	if @player_guess < 1 then puts "Now, don't be difficult. Try again!" 
+	elsif @player_guess > 99 then puts "Now, don't be difficult. Try again!"	
+	elsif @player_guess < @target_number then puts "Sorry, try a higher number!"
+	elsif @player_guess > @target_number then puts "Sorry, try a lower number." 
+	else @got_it_right = true end
+	end 
+
+	if @got_it_right == true then puts "Congratulations #{@name}, you won the game!" + "It only took #{@counter} tries! You're so very clever." end
+end 
+
+end 
+
+
+test = Game.new
+test.intro
+test.set_number
+test.check_for_win
