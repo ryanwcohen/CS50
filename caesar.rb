15f5cf45ac50fb2ea2b554b2c14f3972a474b557
Ryan W. Cohen <ryan.wade.cohen@gmail.com>
2011-07-23 13:35:40 -0700

    Fixed a few bugs. Should work with all digits, positive and negative.

--- caesar.rb
+++ caesar.rb
@@ -14,12 +14,12 @@ end
 
 # encrypts the plaintext, rotating along alphabet by places equal to key
 # has.key?() method leaves punctuation and space formatting unciphered
-# converts uppercase to lowercase as temporary workaround until caps can be implemented
+# converts uppercase to lowercase as temporary workaround until caps can be implemented (separate hash?)
 
 def chiffre 
 
-	@character_to_digit = {'a',1,'b',2,'c',3,'d',4,'e',5,'f',6,'g',7,'h',8,'i',9,'j',10,'k',11,'l',12,'m',13,'n',14,'o',15,'p',16,'q',17,'r',18,'s',19,'t',20,'u',21,'v',22,'w',23,'x',24,'y',25,'z',26}
-	@digit_to_character = {1,'a',2,'b',3,'c',4,'d',5,'e',6,'f',7,'g',8,'h',9,'i',10,'j',11,'k',12,'l',13,'m',14,'n',15,'o',16,'p',17,'q',18,'r',19,'s',20,'t',21,'u',22,'v',23,'w',24,'x',25,'y',26,'z'}
+	@character_to_digit = {'a',0,'b',1,'c',2,'d',3,'e',4,'f',5,'g',6,'h',7,'i',8,'j',9,'k',10,'l',11,'m',12,'n',13,'o',14,'p',15,'q',16,'r',17,'s',18,'t',19,'u',20,'v',21,'w',22,'x',23,'y',24,'z',25}
+	@digit_to_character = {0,'a',1,'b',2,'c',3,'d',4,'e',5,'f',6,'g',7,'h',8,'i',9,'j',10,'k',11,'l',12,'m',13,'n',14,'o',15,'p',16,'q',17,'r',18,'s',19,'t',20,'u',21,'v',22,'w',23,'x',24,'y',25,'z'}
 	
 	@encrypted_text = ""
 	@plaintext.each do |character| 
@@ -27,16 +27,19 @@ def chiffre
 		
 		if @character_to_digit.has_key?(character) == false
 			new_character = character
-		else	original_number = @character_to_digit[character]
-			new_number = original_number.to_i + @key
+		else	original_number = @character_to_digit[character]			
+			@new_number = original_number.to_i + @key
 
-			if new_number > 26
-				new_number = new_number.modulo(26) end
+			if @new_number < 0 
+				@new_number = -1*(@new_number) end
+
+			if @new_number > 25
+				@new_number = @new_number.modulo(26) end
 		
-			new_character = @digit_to_character[new_number]
+			new_character = @digit_to_character[@new_number]
 		end
 		
-		@encrypted_text += new_character 
+		@encrypted_text += new_character.to_s 
 		end
 end 
 
